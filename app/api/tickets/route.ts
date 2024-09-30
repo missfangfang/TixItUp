e34db80e5f87b164a577f23d2b56e0f2d8609d45
missfangfang <tiffanyfang67@gmail.com>
2024-09-29 22:35:46 -0700

    [feat] Add Zod to validate new tickets

---
+++ app/api/tickets/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { ticketSchema } from '@/ValidationSchemas/ticket';
+import prisma from '@/prisma/db';
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const isValid = ticketSchema.safeParse(body);
+
+  if (!isValid.success) {
+    return NextResponse.json(isValid.error.format(), { status: 400 });
+  }
+
+  const newTicket = await prisma.ticket.create({
+    data: { ...body },
+  });
+
+  return NextResponse.json(newTicket, { status: 201 });
+}
