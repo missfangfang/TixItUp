7d748c9ef57ed0302b34c3a697895ea682d52476
missfangfang <tiffanyfang67@gmail.com>
2024-10-03 23:14:53 -0700

    [feat] Add pagination for displaying tickets

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -2,9 +2,21 @@ import prisma from "@/prisma/db";
 import DataTable from "@/app/tickets/DataTable";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import Pagination from "@/components/Pagination";
 
-const Tickets = async () => {
-  const tickets = await prisma.ticket.findMany();
+interface SearchParams {
+  page: string;
+}
+
+const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
+
+  const ticketCount = await prisma.ticket.count();
+  const tickets = await prisma.ticket.findMany({
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
 
   return (
     <div>
@@ -16,6 +28,11 @@ const Tickets = async () => {
         New Ticket
       </Link>
       <DataTable tickets={tickets} />
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
