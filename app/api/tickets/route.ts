b205a6a80601f4e3080238cf5e8a9dd267022fd8
missfangfang <tiffanyfang67@gmail.com>
2024-10-08 00:51:58 -0700

    [feat] Add admin-only permissions to create accounts

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -1,8 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ticketSchema } from "@/ValidationSchemas/ticket";
 import prisma from "@/prisma/db";
+import { getServerSession } from "next-auth";
+import options from "@/app/api/auth/[...nextauth]/options";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+  }
+
   const body = await request.json();
   const isValid = ticketSchema.safeParse(body);
 
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,8 +2,27 @@ import { NextRequest, NextResponse } from "next/server";
 import { userSchema } from "@/ValidationSchemas/user";
 import prisma from "@/prisma/db";
 import bcrypt from "bcryptjs";
+import { getServerSession } from "next-auth";
+import options from "@/app/api/auth/[...nextauth]/options";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(options);
+  if (!session) {
+    return NextResponse.json(
+      { error: "Authenticated failed: Invalid username or password" },
+      { status: 401 }
+    );
+  }
+  if (session.user.role !== "ADMIN") {
+    return NextResponse.json(
+      {
+        error:
+          "Unauthorized: Only admin users are permitted to create new accounts",
+      },
+      { status: 401 }
+    );
+  }
+
   const body = await request.json();
 
   const isValid = userSchema.safeParse(body);
