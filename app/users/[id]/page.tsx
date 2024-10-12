a52d3da29a9fc09bf6283ca33c43c1c688fcc2d7
missfangfang <tiffanyfang67@gmail.com>
2024-10-11 20:20:51 -0700

    [feat] Require admin permission to view and edit users

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,11 +1,18 @@
 import prisma from "@/prisma/db";
 import UserForm from "@/components/UserForm";
+import { getServerSession } from "next-auth";
+import options from "@/app/api/auth/[...nextauth]/options";
 
 interface Props {
   params: { id: string };
 }
 
 const EditUser = async ({ params }: Props) => {
+  const session = await getServerSession(options);
+  if (session?.user.role !== "ADMIN") {
+    return <p className="text-destructive">Admin Access Required</p>;
+  }
+
   const user = await prisma?.user.findUnique({
     where: { id: parseInt(params.id) },
   });
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,16 @@
 import UserForm from "@/components/UserForm";
 import UsersDataTable from "@/app/users/UsersDataTable";
 import prisma from "@/prisma/db";
+import { getServerSession } from "next-auth";
+import options from "@/app/api/auth/[...nextauth]/options";
 
 const Users = async () => {
+  const session = await getServerSession(options);
   const users = await prisma.user.findMany();
+
+  if (session?.user.role !== "ADMIN") {
+    return <p className="text-destructive">Admin Access Required</p>;
+  }
 
   return (
     <div>
